0339be6e9f3f938f3955648b464e4a538811674d
Mariam Blanco <mariamblanko@yahoo.es>
2023-10-22 20:07:21 +0200

    Change fake server URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-   baseURL: 'https://fake-server-github-jobs-app.herokuapp.com',
-   withCredentials: false,
-   headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-   },
+  baseURL: 'https://server-github-jobs-production.up.railway.app',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 });
 
-export const getData = params => apiClient.get(params);
+export const getData = (params) => apiClient.get(params);
 
 export const getJobs = () => apiClient.get('/jobs');
 
-export const getJob = id => apiClient.get('/jobs/' + id);
+export const getJob = (id) => apiClient.get('/jobs/' + id);
